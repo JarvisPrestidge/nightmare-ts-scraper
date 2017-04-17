5cfc054838ae37ef3df9fc9ef1bc5f14933ca917
Jarvis Prestidge <jarvisprestidge@gmail.com>
2017-04-17 15:43:41 +0100

    Setup project structure.

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+var StartUp = (function () {
+    function StartUp() {
+    }
+    StartUp.main = function () {
+        console.log('Hello world!');
+        return 0;
+    };
+    return StartUp;
+}());
+StartUp.main();
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+class StartUp {
+    public static main(): number {
+        console.log('Hello world!')
+        return 0
+    }
+}
+
+StartUp.main()
